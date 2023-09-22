7807b2ecdc2e9819be7702154d7c3b84d1eb525d
CataVergara <134965706+CataVergara@users.noreply.github.com>
2023-09-22 17:53:15 -0300

    Update procesador_formulario.php

--- procesador_formulario.php
+++ procesador_formulario.php
@@ -4,20 +4,20 @@ $username = "tu_usuario_mysql";
 $password = "tu_contraseña_mysql";
 $dbname = "mi_basedatos";
 
-// Crear conexión
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
+
 if ($conn->connect_error) {
     die("Error de conexión a la base de datos: " . $conn->connect_error);
 }
 
-// Obtener datos del formulario
+
 $nombre = $_POST['nombre'];
 $correo = $_POST['correo'];
 $mensaje = $_POST['mensaje'];
 
-// Insertar los datos en la base de datos
+
 $sql = "INSERT INTO contactos (nombre, correo, mensaje) VALUES ('$nombre', '$correo', '$mensaje')";
 
 if ($conn->query($sql) === TRUE) {
