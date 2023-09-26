21a46709a0183682e231deca3a66a944e194a109
VicenteAZB <vicente.alvarez2023@alu.uct.cl>
2023-09-26 20:01:20 -0300

    26SEP

--- PHP/contacto.php
+++ PHP/contacto.php
@@ -15,7 +15,7 @@ if (isset($_SESSION['usuario'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login & Register Encanto Araucano</title>
-    <link rel="stylesheet" href="../css/form.css">
+    <link rel="stylesheet" href="../Css/form.css">
 </head>
 
 <body>
